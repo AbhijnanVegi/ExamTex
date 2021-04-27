d117eb2b4137fdd6d528395971c699903daf48ae
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-04-27 22:25:13 +0530

    Improved quality of code
    Replaced repetitive code and added comments for clarity

--- Utils/parsers.c
+++ Utils/parsers.c
@@ -1,6 +1,7 @@
 #include "parsers.h"
 #include "lineNumber.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -9,122 +10,138 @@ int isSyntax(char c)
     return (c == '<' || c == '>' || c == '=' || c == '{' || c == '}');
 }
 
-
-void parseType(FILE *fp,char dest[])
+void raiseSyntaxError(char c)
 {
-    //Reads the type of block,i.e, question and sample and stores it 
+    printf("Unexpected character '%c' at line : %d", lineNumber);
+    exit(1);
+}
+
+void parseType(FILE *fp, char dest[])
+{
+    //Reads the type of block,i.e, question and sample and stores it
     //in the char array passed in
-    char c;//for storing the character read
-    flag inWord = false;//Flags if any character's been read
-    int index = 0;//Index for iterating over dest[] to store characters
-    while((c=fgetc(fp))!='{')
+    char c;              //for storing the character read
+    flag inWord = false; //Flags if any character's been read
+    int index = 0;       //Index for iterating over dest[] to store characters
+    while ((c = fgetc(fp)))
     {
-        if(!(isSyntax(c)) && c!=' ')
+        if (!(isSyntax(c)) && !(c == ' ' || c == '{' || c == '\n')) //Check if given character is valid
         {
             inWord = true;
             dest[index] = c;
             index++;
         }
-        else if (isSyntax(c))
+        else if (isSyntax(c) && c != '{') //Character part of syntax  and not '{'
         {
-            printf("Unexpected symbol '%c' at line number : %d",c,lineNumber);
+            raiseSyntaxError(c);
         }
-        else if (inWord)//There's an ' ' so break here
+        else if (inWord || c == '{') //Stop when ' ' comes after word or reads '{'
         {
             dest[index] = '\0';
-            fseek(fp,-1L,SEEK_CUR);//set the cursor to prev cursor so it can be read
-            break;                 //by other functions
+            fseek(fp, -1L, SEEK_CUR); //set the cursor to prev cursor so it can be read
+            break;                    //by other functions
         }
-        if (c == '\n') lineNumber++;
+        if (c == '\n')
+            lineNumber++;
     }
 }
 
-void parseArgument(FILE *fp,char parameter[],char value[])
+void parseArgument(FILE *fp, char parameter[], char value[])
 {
     //Reads the parameter and its value, and stores them in the given array
-    char c;//For storing character read
-    flag inParam = false;//Flags if parameter is being/been read
-    flag inVal = false;//Flags if value is being/been read
+    char c;               //For storing character read
+    flag inParam = false; //Flags if parameter is being/been read
+    flag inVal = false;   //Flags if value is being/been read
     flag escape = false;
-    int index = 0;//index for filling the arrays
-    while ((c = fgetc(fp))!='{')//Move cursor to the start of argument
+    int index = 0;                 //index for filling the arrays
+    while ((c = fgetc(fp)) != '{') //Move cursor to the start of argument
     {
         if (isSyntax(c) && c != '>')
         {
-            printf("Unexpected char '%c' at line number : %d",c,lineNumber);
+            raiseSyntaxError(c);
         }
-        else if (c == '>')//Couldn't find a '{', No argument to read
+        else if (c == '>') //Couldn't find a '{', No argument to read
         {
-            strcpy(parameter,"NULL");//return NULL in both if missing argument
-            strcpy(value,"NULL");
+            strcpy(parameter, "NULL"); //return NULL in both if missing argument
+            strcpy(value, "NULL");
             return;
         }
-        
-        if (c != ' ' && c != '\n')
+
+        if (c != ' ' && c != '\n') //Unexpected character
         {
-            printf("Unexpected char '%c' at line number : %d",c,lineNumber);
-            exit(1);
+            raiseSyntaxError(c);
         }
-        if (c == '\n') lineNumber++;
+        if (c == '\n')
+            lineNumber++;
     }
-    while ((c = fgetc(fp)))//Get and fill parameter
+    while ((c = fgetc(fp))) //Get and fill parameter
     {
-        if (!(isSyntax(c)) && c != ' ')
+        if (!(isSyntax(c)) && !(c == ' ' || c == '\n')) //Fillin if valid char
         {
             inParam = true;
             parameter[index] = c;
             index++;
         }
-        else if (isSyntax(c) && c!= '=')
+        else if (isSyntax(c) && c != '=') //Raise syntax error if character is not '='
         {
-            printf("Unexpected char '%c' at line number : %d",c,lineNumber);
-            exit(1);
+            raiseSyntaxError(c);
         }
-        else if (inParam)//space after parameter
+        else if (inParam) //space after parameter
         {
             parameter[index] = '\0';
-            fseek(fp,-1L,SEEK_CUR);
+            fseek(fp, -1L, SEEK_CUR);
             break;
         }
-        if (c == '\n') lineNumber++;
+        if (c == '\n')
+            lineNumber++;
     }
-    index = 0;//reset index
-    while ((c=fgetc(fp))!='=')//Move cursor to '='
+    index = 0;                     //reset index
+    while ((c = fgetc(fp)) != '=') //Move cursor to '='
     {
-        if(c == '}');
+        if (c == '}')
+            ; //Raise error as '=' is expected
         {
-            printf("Expected '=' in line number : %d",lineNumber);
+            printf("Expected '=' in line number : %d", lineNumber);
             exit(1);
         }
-        if (c == '\n')lineNumber++;
-        if (c != ' ' && c!='\n')
+        if (c != ' ' && c != '\n') //Excess character so raise syntax error
         {
-            printf("Unexpected char '%c' at line number : %d",c,lineNumber);
-            exit(1);
+            raiseSyntaxError(c);
         }
+        if (c == '\n')
+            lineNumber++;
     }
-    while ((c = fgetc(fp)))//Fill in value after considering escape sequences
+    while ((c = fgetc(fp))) //Fill in value after considering escape sequences
     {
-        if(!escape && c == '}')
+        if (!escape && c == '}') //Unescaped '}' finishes the argument
         {
+            value[index] = '\0';
+            fseek(fp, -1L, SEEK_CUR);
             break;
         }
-        else if (!escape && isSyntax(c))
+        else if (!escape && isSyntax(c)) //Unescaped syntax character raises error
         {
-            printf("Unexpected char '%c' at line number : %d",c,lineNumber);
-            exit(1);
+            raiseSyntaxError(c);
         }
-        else if(c == '\\')
+        else if (c == '\\') // '\' escapes the next char
         {
             escape = true;
+            inVal = true;
+            continue;
+        }
+        else if (!(inVal) && (c == ' ' || c == '\n')) //Ignore all the spaces and newlines before first accepted character
+        {
             continue;
         }
         else
         {
-            value[index] = c;
+            value[index] = c; // The character is valid Thus fill it
+            inVal = true;
             index++;
         }
-        if (escape) escape = false;
-        if (c == '\n') lineNumber++;
+        if (escape) //Toggle escape if true
+            escape = false;
+        if (c == '\n')
+            lineNumber++;
     }
 }
