49d4ed40d72c1a334a8853ba8476a6a0669f875d
Itreek <82515521+Itreek@users.noreply.github.com>
2021-05-08 13:29:47 +0530

    Add files via upload

---
+++ print_output.h
@@ -0,0 +1,11 @@
+#ifndef printTF
+#define printTF
+#include<stdio.h>
+#include<stdlib.h>
+#include "vec1.h"
+
+void print_op(FILE* qb,FILE* outputPaper, FILE* outputAnswer);
+void go_to_pointer(FILE *qb, int n, FILE *outputPaper , FILE* outputAnswer);
+void printTF(vector* v);
+
+#endif
