9192ba3f15a0f3381aa9d158a1cb8585363011f5
sumanth <sumanth369.vuddagiri@gmail.com>
2021-05-08 15:34:52 +0530

    Validator updated

--- Validators/validateOneWord.c
+++ Validators/validateOneWord.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include "../Utils/parsers.h"
@@ -9,12 +10,22 @@ void validateOneWord(FILE *fp)
 {
     char param[20];
     char value[100];
-    // char text[100];
-    
+        
     int numberOfParametersRequired = 4;
     int parametersRead = 0;
-    // char ans[100];
-    
+    enum parameters
+    {
+        TEXT,
+        ANS,
+        DIFFICULTY,
+        SCORE
+    };
+    bool isParameterRead[4]; // Array to store the presence of parameters
+    isParameterRead[TEXT] = false;
+    isParameterRead[ANS] = false;
+    isParameterRead[DIFFICULTY] = false;
+    isParameterRead[SCORE] = false;
+
     double difficulty;
     double score;
     
@@ -23,39 +34,88 @@ void validateOneWord(FILE *fp)
         if(strcmp(param,"text") == 0)
         {
             // Type checking not needed for question
-            parametersRead++;
+            if(!isParameterRead[TEXT])
+            {
+                isParameterRead[TEXT] = true;
+                parametersRead++;
+            }
+            // Since parameter already read, raising duplicate error
+            else{
+                printf("Error on line number : %d : Duplicate parameter \"text\"", lineNumber);
+                exit(1);
+            }
+            
         }
-        
         else if (strcmp(param,"ans") == 0)
         {
             // Same here, type checking not needed since it can be any type in single or group of words
-            
-            parametersRead++;
+            if(!isParameterRead[ANS])
+            {
+                isParameterRead[ANS] = true;
+                parametersRead++;
+            }
+            else
+            {
+                printf("Error on line number : %d : Duplicate parameter \"ans\"", lineNumber);
+                exit(1);
+            }
         }
         else if (strcmp(param,"difficulty") == 0)
         {
             // Difficulty can be in double, checking if it is the same in input
-            if(sscanf(value,"%lf",&difficulty)!=1)
+            if(!isParameterRead[DIFFICULTY])
             {
-                printf("Error on line number : %d, Difficulty must be decimal",lineNumber);
+                if(sscanf(value,"%lf",&difficulty)!=1)
+                {
+                    printf("Error on line number : %d, Difficulty must be decimal",lineNumber);
+                    exit(1);
+                }
+                isParameterRead[DIFFICULTY] = true;
+                parametersRead++;
             }
-            parametersRead++;
+            else{
+                printf("Error on line number %d: Duplicate parameter \"difficulty\"", lineNumber);
+                exit(1);
+            }
+            
+            
         }
         else if (strcmp(param,"score") == 0)
         {
             //  Score can be awarded only in decimals according to agreed format
-            
-            if(sscanf(value,"%lf",&score)!=1)
+            if(!isParameterRead[SCORE])
             {
-                printf("Error on line number : %d, Difficulty must be decimal",lineNumber);
+                if(sscanf(value,"%lf",&score)!=1)
+                {
+                    printf("Error on line number : %d, Difficulty must be decimal",lineNumber);
+                    exit(1);
+                }
+                isParameterRead[SCORE] = true;
+                parametersRead++;
             }
-            parametersRead++;
+            else
+            {
+                printf("Error on line number : %d: Duplicate parameter \"score\"",lineNumber);
+                exit(1);
+            }
+            
         }
         else
         {
             // Checking if any other paramter is found other than above specified
             printf("Unknown parameter '%s' on line number : %d",param,lineNumber);
         }
-        
+    }
+    if(parametersRead!=numberOfParametersRequired)
+    {
+        printf("Error on line number : %d: Missing parameters : ",lineNumber);//print all missing parameters
+        if (!isParameterRead[TEXT])
+            printf("\"text\" ");
+        if (!isParameterRead[ANS])
+            printf("\"ans\" ");
+        if (!isParameterRead[DIFFICULTY])
+            printf("\"diffuculty\" ");
+        if (!isParameterRead[SCORE])
+            printf("\"score\" ");
     }
 }
