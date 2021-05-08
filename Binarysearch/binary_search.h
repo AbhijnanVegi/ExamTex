161f3ca32061a6fe3ddbc764d980cccd4c149c57
Itreek <82515521+Itreek@users.noreply.github.com>
2021-05-08 19:12:26 +0530

    Create binary_search.h

---
+++ Binarysearch/binary_search.h
@@ -0,0 +1,8 @@
+#ifndef _Binary_Search
+#define _Binary_Search
+#include "vec1.h"
+
+int binary_ub_search(vector *v, double upper_limit);
+int binary_lb_search(vector *v, double lower_limit);
+
+#endif
