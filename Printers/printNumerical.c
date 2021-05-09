72eff73d5fc909379202bbbfc4091c5d69f26a3a
Abhijnan Vegi <68379071+AbhijnanVegi@users.noreply.github.com>
2021-05-09 15:01:20 +0530

    Compatibility update

--- Printers/printNumerical.c
+++ Printers/printNumerical.c
@@ -7,33 +7,33 @@
 
 void printNumerical(FILE* qb,FILE* outputPaper, FILE* outputAnswer)
 {
-    char **param;
-    char **value;
+    char *param;
+    char *value;
     char *text;
     double score;
     double ans;
-    while (parseArgument(qb,param,value))//Get all parameter values and store them
+    while (parseArgument(qb,&param,&value))//Get all parameter values and store them
     {
-        if (strcmp(*param,"text") == 0)
+        if (strcmp(param,"text") == 0)
         {
-            clean(*value);
-            text = *value;
+            clean(value);
+            text = value;
             free(*param);
         }
-        else if (strcmp(*param,"score") == 0)
+        else if (strcmp(param,"score") == 0)
         {
-            score = atof(*value);
-            free(*param);
-            free(*value);
+            score = atof(value);
+            free(param);
+            free(value);
         }
-        else if (strcmp(*param,"answer") == 0)
+        else if (strcmp(param,"answer") == 0)
         {
-            ans = atof(*value);
-            free(*param);
-            free(*value);
+            ans = atof(value);
+            free(param);
+            free(value);
         }
     }
-    fprintf(outputPaper,"%s",*text);//Print questions to question paper
+    fprintf(outputPaper,"%s",text);//Print questions to question paper
     fprintf(outputPaper,"\t[score : %0.1f]\n",score);
     fprintf(outputPaper,"Answer : ____\n");
     fprintf(outputAnswer,"%0.2f",ans);//Print answer to answer key
