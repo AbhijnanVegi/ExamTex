0c4e104ca1e557700cc135c788b7cd2782197b39
Itreek <82515521+Itreek@users.noreply.github.com>
2021-05-09 18:14:26 +0530

    Update printTF_output.c

--- Printers/printTF_output.c
+++ Printers/printTF_output.c
@@ -2,38 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "vec1.h"
-#include "truefalse.h"
-#include "parsers.h"
-#include "lineNumber.h"
+#include "../Utils/parsers.h"
+#include "../Utils/cleanText.h"
 
 void printTF(FILE *qb, FILE *outputPaper, FILE *outputAnswer)
 {
-    char **param;
-    char **value;
+    char *param;
+    char *value;
     char *text;
     double score;
     double ans;
-    while (parseArgument(qb, param, value))
+    while (parseArgument(qb, &param, &value))
     {
         if (strcmp(*param, "text") == 0)
         {
-            clean(*value);
-            text = *value;
+            clean(value);
+            text = value;
             //strcpy(text, value);
-            free(*param);
+            free(param);
         }
-        else if (strcmp(*param, "score") == 0)
+        else if (strcmp(param, "score") == 0)
         {
-            score = atof(*value);
-            free(*param);
-            free(*value);
+            score = atof(value);
+            free(param);
+            free(value);
         }
-        else if (strcmp(*param, "ans") == 0)
+        else if (strcmp(param, "ans") == 0)
         {
-            ans = atof(*value);
-            free(*param);
-            free(*value);
+            ans = atof(value);
+            free(param);
+            free(value);
         }
     }
     fprintf(outputPaper, "%s", text);
@@ -42,6 +40,5 @@ void printTF(FILE *qb, FILE *outputPaper, FILE *outputAnswer)
     fprintf(outputPaper, "True\n");
     fprintf(outputPaper, "False\n");
     fprintf(outputAnswer, "%0.2f", ans);
+    free(text);
 }
-
-
