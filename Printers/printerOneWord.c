25a50efc8f0eb142f2962a35b7f326cef853e91e
sumanth <sumanth369.vuddagiri@gmail.com>
2021-05-09 21:28:36 +0530

    .

--- Printers/printerOneWord.c
+++ Printers/printerOneWord.c
@@ -1,53 +1,51 @@
-#include <stdio.h>
-
-#include <stdlib.h>
+//UPDATED...
+#include "../Validators/validateOneWord.h"
 
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include "../Utils/parsers.h"
-
+#include "../vector/vec.h"
+#include "../Utils/cleanText.h"
 #include "../Utils/lineNumber.h"
+#include "../Utils/parsers.h"
+#include "../Utils/readFile.h"
 
-// Parameters: qPaper - Output for question paper, finAnswer - file to store answer
-void printOneWord(FILE *fp, FILE* qPaper, FILE* finAnswer)
+// Parameters: qPaper - Output for question paper, finAnswer - Output for store answer
+void printOneWord(FILE *fp, FILE *qPaper, FILE *finAnswer)
 {
+    char *text;
+    // stroring type of "param" and "value" associated with it
     char *param;
     char *value;
-    char *text;
-    
-    char *ans;
-    double score;
-    
-    while(parseArgument(fp,&param,&value))
-    {
-        if(strcmp(param,"text") == 0)
-        {
-            clean(value);
-            text = value;// Storing the "question"
-            free(param);
-            free(value);
-        }
-        else if (strcmp(param,"ans") == 0)
-        {
-            clean(value);
-            ans = value; // Storing the "answer"
-            free(param);
-            
-        }
-        else if (strcmp(param,"score") == 0)
-        { 
-            score = atof(value); // Stroring the "score"
-            free(param);
-            free(value);
-        }        
-    }
-    // Output questions in proper format to question paper
-    fprintf(qPaper, "%s", *text);
-    fprintf(qPaper, "\t[score : %0.1f]\n", score);
-    fprintf(qPaper, "Answer: ____\n");
-    // Output answers to answer key
-    fprintf(finAnswer, "%s", *ans);
 
-    free(text);
-    free(ans);
+    float diff, score;
+    char *answer;
+    while (parseArgument(fp, &param, &value))
+    {
+        if (strcmp(param, "text") == 0)
+        {
+            clean(value);
+            text = (char *)malloc(sizeof(strlen(value)));
+            strcpy(text, value); // Storing the "question"
+        }
+        else if (strcmp(param, "ans") == 0)
+        {
+            clean(value);
+            answer = (char *)malloc(sizeof(char) * strlen(value));
+            strcpy(answer, value); // Storing the "answer"
+            // Removes spaces from answer
+            remove_spaces(&answer);
+        }
+        else if (strcmp(param, "score") == 0)
+        {
+            score = atof(value); // Stroring the "score"
+        }
+    }
+    // Output to question paper in proper format
+    fprintf(qPaper, "Q) %s", text);
+    fprintf(qPaper, "\t[score : %0.2f]\n", score);
+    fprintf(qPaper, "Answer: _________\n");
+    // Output answer to answers key
+    fprintf(finAnswer, "Correct Ans : %s", answer);
 }
--- Validators/validateOneWord.c
+++ Validators/validateOneWord.c
@@ -1,151 +1,125 @@
-#include <stdio.h>
+#include "validateOneWord.h"
 
+#include <string.h>
 #include <stdlib.h>
 
-#include <stdbool.h>
-
-#include "../Utils/parsers.h"
-
+#include "../vector/vec.h"
+#include "../Utils/cleanText.h"
 #include "../Utils/lineNumber.h"
+#include "../Utils/parsers.h"
+#include "../Utils/readFile.h"
+
+extern vector vec_one_word;
 
 void validateOneWord(FILE *fp, int id)
 {
-    char *param;
-    char *value;
-        
-    int numberOfParametersRequired = 4;
+    char *param; // To store the type of parameter
+    char *value; // To store the value associated with parameter.
+
+    int count = 4;
     int parametersRead = 0;
-    // Parameters Needed
-    //      =>text
-    //      =>ans
-    //      =>difficulty
-    //      =>score
-    bool isParameterRead[4]; // Array to store the presence of parameters
 
-    enum parameters
-    {
-        TEXT,
-        ANS,
-        DIFFICULTY,
-        SCORE
-    };
-
-    int z = 4;
-    while(z--){
-        isParameterRead[z] = false;
-    }
-
-    double difficulty;
-    double score;
+    // Flags to check any repetitive params
+    bool text_flag = false, diff_flag = false, score_flag = false, ans_flag = false;
+    float diff, score;
     
-    while(parseArgument(fp,&param,&value))
+    parameterUnion u;
+    u.nd.id = id;
+
+    while (parseArgument(fp, &param, &value))
     {
-        if(strcmp(param,"text") == 0)
+        // parseArgument(fp, &param, &value);
+        if (strcmp(param, "text") == 0)
         {
             // Type checking not needed for question
-            if(!isParameterRead[TEXT])
+            if (!text_flag)
             {
-                isParameterRead[TEXT] = true;
-                parametersRead++;
+                text_flag = true;
             }
-            // Since parameter already read, raising duplicate error
-            else{
-                printf("Error on line number : %d : Duplicate parameter \"text\"", lineNumber);
-                free(param);
-                free(value);
-                exit(1);                
+            else // checking for duplicate appearances of parameters
+            {
+                printf("Error! Text initialised twice in line no : %d \n", lineNumber);
+                exit(1);
             }
-            free(param);
-            free(value);
         }
-        else if (strcmp(param,"ans") == 0)
+        else if (strcmp(param, "ans") == 0) // Validation for ans param
         {
             // Same here, type checking not needed since it can be any type in single or group of words
-            if(!isParameterRead[ANS])
+            if (!ans_flag)
             {
-                isParameterRead[ANS] = true;
-                parametersRead++;
+                ans_flag = true;
             }
             else
             {
-                printf("Error on line number : %d : Duplicate parameter \"ans\"", lineNumber);
-                free(param);
-                free(value);
+                printf("Error! Answer initialised twice in line no : %d \n", lineNumber);
                 exit(1);
             }
         }
-        else if (strcmp(param,"difficulty") == 0)
+        else if (strcmp(param, "difficulty") == 0) // Validation for difficulty param
         {
             // Difficulty can be in double, checking if it is the same in input
-            if(!isParameterRead[DIFFICULTY])
+            if (!diff_flag)
             {
-                if(sscanf(value,"%lf",&difficulty)!=1)
+                diff_flag = true;
+                if (sscanf(value, "%f", &diff) != 1)
                 {
-                    printf("Error on line number : %d, Difficulty must be decimal",lineNumber);
-                    free(param);
-                    free(value);
+                    printf("Error on line number : %d, Answer must be decimal", lineNumber);
                     exit(1);
                 }
-                isParameterRead[DIFFICULTY] = true;
-                parametersRead++;
-            }
-            else{
-                printf("Error on line number %d: Duplicate parameter \"difficulty\"", lineNumber);
-                free(param);
-                free(value);
-                exit(1);
-            }
-            free(param);
-            free(value);
-            
-        }
-        else if (strcmp(param,"score") == 0)
-        {
-            //  Score can be awarded only in decimals according to agreed format
-            if(!isParameterRead[SCORE])
-            {
-                if(sscanf(value,"%lf",&score)!=1)
-                {
-                    printf("Error on line number : %d, Difficulty must be decimal",lineNumber);
-                    free(param);
-                    free(value);
-                    exit(1);
-                }
-                isParameterRead[SCORE] = true;
-                parametersRead++;
             }
             else
             {
-                printf("Error on line number : %d: Duplicate parameter \"score\"",lineNumber);
-                free(param);
-                free(value);
+                printf("Error! Difficulty initialised twice in line no : %d \n", lineNumber);
                 exit(1);
             }
-            free(param);
-            free(value);
+        }
+        else if (strcmp(param, "score") == 0) // Validation for score param
+        {
+            if (!score_flag)
+            {
+                score_flag = true;
+                if (sscanf(value, "%f", &score) != 1)
+                {
+                    printf("Error on line number : %d, Difficulty must be decimal", lineNumber);
+                }
+            }
+            else
+            {
+                printf("Error! Score initialised twice in line no : %d \n", lineNumber);
+                exit(1);
+            }
         }
         else
         {
             // Checking if any other paramter is found other than above specified
-            printf("Unknown parameter '%s' on line number : %d",param,lineNumber);
-            free(param);
-            free(value);
+            printf("Unknown parameter '%s' on line number : %d", param, lineNumber);
             exit(1);
         }
     }
-    if(parametersRead!=numberOfParametersRequired)
+    // Checking if any of the params didn't appear even once
+    // Output those as errors
+    if (!ans_flag)
     {
-        printf("Error on line number : %d: Missing parameters : ",lineNumber);//print all missing parameters
-        if (!isParameterRead[TEXT])
-            printf("\"text\" ");
-        if (!isParameterRead[ANS])
-            printf("\"ans\" ");
-        if (!isParameterRead[DIFFICULTY])
-            printf("\"diffuculty\" ");
-        if (!isParameterRead[SCORE])
-            printf("\"score\" ");
-        free(param);
-        free(value);
+        printf("Error! Not initialised answer in line no : %d\n", lineNumber);
         exit(1);
     }
+    if (!text_flag)
+    {
+        printf("Error! Not initialised question in line no : %d\n", lineNumber);
+        exit(1);
+    }
+    if (!score_flag)
+    {
+        printf("Error! Not initialised score in line no : %d\n", lineNumber);
+        exit(1);
+    }
+    if (!diff_flag)
+    {
+        printf("Error! Not initialised difficulty in line no : %d\n", lineNumber);
+        exit(1);
+    }
+    // Storing these useful params
+    u.nd.diff = diff;
+    u.nd.score = score;
+    push_back(&vec_one_word, u);
 }
